cc57e77f1e658b3f92bc934ac5eb990fa860b773
lujie <lujie@4399inc.com>
2020-12-12 20:35:10 +0800

    fix

--- data/code/Excuses/run_w2v.py
+++ data/code/Excuses/run_w2v.py
@@ -18,8 +18,7 @@ if __name__ == '__main__':
 
     args = parse_command_params()
 
-    if not os.path.exists(args['user_path']):
-        os.makedirs(args['user_path'])
+    if not os.path.exists(os.path.join(args['user_path'], 'w2v')):
         os.makedirs(os.path.join(args['user_path'], 'w2v'))
 
     attr = pd.read_csv('/data/raw_data/attr.txt', sep='\t', names=['linkid', 'length', 'direction', 'pathclass', 'speedclass', 'LaneNum', 'speedlimit', 'level', 'width'])
@@ -40,7 +39,7 @@ if __name__ == '__main__':
     fw.close()
 
     model = Word2Vec(LineSentence(os.path.join(args['user_path'], 'topo_w2v_train.txt')), alpha=0.02, size=64, window=3, min_count=2, sg=1, hs=1, workers=multiprocessing.cpu_count())
-    model.save(os.path.join(args['user_path'], 'w2v/topo.model'))
+    # model.save(os.path.join(args['user_path'], 'w2v/topo.model'))
 
     data = pd.concat([pd.DataFrame(model.wv.index2word, columns=['linkid'], dtype=int), pd.DataFrame(model.wv.vectors)], axis=1)
     ind = pd.DataFrame(linkids, columns=['linkid'])
